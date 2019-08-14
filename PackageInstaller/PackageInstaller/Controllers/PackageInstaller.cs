39a34186aaa7b6631973eccd123bdc886050adf1
Unknown <jonathanpedregon@gmail.com>
2019-08-13 20:17:41 -0600

    Creating solution

---
+++ PackageInstaller/PackageInstaller/Controllers/PackageInstaller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageInstaller.Controllers
+{
+    public interface IPackageInstaller
+    {
+        string InstallPackages(string userInput);
+    }
+
+    public class PackageInstaller : IPackageInstaller
+    {
+        public string InstallPackages(string userInput)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PackageInstaller/PackageInstaller/Program.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using PackageInstaller.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageInstaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var containerBuilder = new ContainerBuilder();
+            var assembly = Assembly.GetExecutingAssembly();
+            containerBuilder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces();
+            var container = containerBuilder.Build();
+            var packageInstaller = container.Resolve<IPackageInstaller>();
+            var output = packageInstaller.InstallPackages(args[0]);
+        }
+    }
+}
