419f5b2a8ef911af3c89520181bccec666be84e9
Unknown <jonathanpedregon@gmail.com>
2019-08-17 09:14:28 -0600

    Adding failing test

---
+++ PackageInstaller/PackageInstaller/Handlers/PackageSorter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using PackageInstaller.Models;
+
+namespace PackageInstaller.Handlers
+{
+    public interface IPackageSorter
+    {
+        IList<string> GetOrderedPackages(IList<Package> packages);
+    }
+
+    public class PackageSorter : IPackageSorter
+
+    {
+        public IList<string> GetOrderedPackages(IList<Package> packages)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PackageInstaller/PackageInstallerTests/Handlers/PackageSorterTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PackageInstaller.Handlers;
+using PackageInstaller.Models;
+
+namespace PackageInstallerTests.Handlers
+{
+    [TestFixture]
+    public class PackageSorterTests
+    {
+        private PackageSorter PackageSorter { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            PackageSorter = new PackageSorter();
+        }
+
+        [Test]
+        public void GetOrderedPackages_FirstExample()
+        {
+            var packages = new List<Package>
+            {
+                new Package("KittenService", "CamelCaser"),
+                new Package("CamelCaser", "")
+            };
+
+            var orderedPackages = PackageSorter.GetOrderedPackages(packages);
+
+            Assert.AreEqual(2, orderedPackages.Count);
+            Assert.AreEqual("CamelCaser", orderedPackages.ElementAt(0));
+            Assert.AreEqual("KittenService", orderedPackages.ElementAt(0));
+        }
+    }
+}
