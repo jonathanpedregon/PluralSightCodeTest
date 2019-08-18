4668d10434eba6a9004a74237185b6865e19168c
Unknown <jonathanpedregon@gmail.com>
2019-08-18 14:41:06 -0600

    Adding failing test

---
+++ PackageInstaller/PackageInstaller/Handlers/PackageStringGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PackageInstaller.Handlers
+{
+    public interface IPackageStringGenerator
+    {
+        string GeneratePackageString(IList<string> packageList);
+    }
+
+    public class PackageStringGenerator : IPackageStringGenerator
+    {
+        public string GeneratePackageString(IList<string> packageList)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
